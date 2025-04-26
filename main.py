de5d08d259ff2b930fb19a849128b223f0d69d22
Sarus1997 <saharat.rus@gmail.com>
2025-04-26 16:28:21 +0700

    update

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from checker.headers import check_security_web
 
 
 if __name__ == "__main__":
-    url = "https://sarus.vercel.app/"  # You can change this to test other URLs
+    url = "https://sarus.vercel.app/"  # เปลี่ยน URL ที่ต้องการตรวจสอบ
     check_security_web(url)
